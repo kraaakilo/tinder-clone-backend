d62775252355b94fee70343e41f7d8932ea70748
Marc-Elvis <kraaakilo@gmail.com>
2024-12-25 19:44:37 +0100

    Wed Dec 25 07:44:37 PM WAT 2024

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,9 @@ use App\Http\Controllers\MessageController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UploadMediaController;
 use App\Http\Controllers\UserController;
-use App\Http\Resources\UserResource;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/", function () {
-    return response()->json(["message" => UserResource::collection(User::all())]);
     return response()->json(["message" => "Service is running!"]);
 });
 
